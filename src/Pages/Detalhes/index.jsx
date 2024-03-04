b3960d0aaaffacdb4f1866e44766c8742e4dfdab
silbjava <silvanomth@gmail.com>
2024-03-04 16:45:56 -0300

    atualização irede

--- src/Pages/Detalhes/index.jsx
+++ src/Pages/Detalhes/index.jsx
@@ -6,16 +6,16 @@ export default function Detalhes() {
     return (
         <>
             <Header />
-            <section className="flex flex-wrap pl-[18vw] pt-[9vw] pb-[12vw] pr-[10vw gap-[5vw]">
+            <section className="flex flex-col items-center sm:flex-row sm:items-start sm:pl-[18vw] pt-[9vw] pb-[12vw] sm:pr-[10vw] gap-[5vw]">
                 <div className="flex flex-col">
-                    <img className="hidden sl:flex w-[16vw] h-[18vw]" src="./../src/assets/SmartWatch.png" alt="smartwatch" />
+                    <img className="hidden sl:flex w-[18vw] h-[20vw]" src="./../src/assets/SmartWatch.png" alt="smartwatch" />
                     <p className="font-inter font-semibold text-px14 text-blue-900 leading-29.05 pt-1">Quantidade disponível</p>
-                    <p className="font-inter font-medium sl:text-px24 text-stone-500 leading-29.05 pt-2">100 itens disponíveis</p>
+                    <p className="font-inter font-medium sl:text-px20 text-stone-500 leading-29.05 pt-2">100 itens disponíveis</p>
                 </div>
                 <div className="flex flex-col ">
                     <h1 className=" font-inter font-semibold text-px24 sm:text-px40 leading-29.05 sm:leading-48.4 text-blue-900">Smart Watt</h1>
-                    <p className="font-inter font-medium text-px16 sm:text-px24 text-stone-500 leading-19.36 sm:leading-29.05">Relógio</p>
-                    <p className=" w-[35vw]  font-inter font-medium  text-px14 lg:text-px24 text-stone-500 leading-16.94 sm:leading-29.05"> O smartwatch é o seu companheiro ideal para uma
+                    <p className="font-inter font-medium text-px16 sm:text-px20 text-stone-500 leading-19.36 sm:leading-29.05">Relógio</p>
+                    <p className=" w-64 sm:w-[35vw]  font-inter font-medium  text-px14 lg:text-px24 text-stone-500 leading-16.94 sm:leading-29.05"> O smartwatch é o seu companheiro ideal para uma
                         vida mais saudável, conectada e cheia de estilo.
                         Com ele, você recebe notificações do seu smartphone,
                         monitora sua saúde (batimentos cardíacos, pressão arterial,
